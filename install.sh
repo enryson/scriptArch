4f361187358714bd97e90a33760cd2045b22d876
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-12 23:27:42 -0300

    Update 2 scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
@@ -13,28 +15,6 @@ pacman -Syy
 pacstrap /mnt base linux linux-firmware base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
-pacman -Syy
-pacman -S reflector --noconfirm
-reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
+cp installChroot.sh
 
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-echo LANG=en_US.UTF-8 >> /etc/locale.conf
-locale-gen
-
-echo ArchLinux >> /etc/hostname
-echo 127.0.0.1	localhost >> /etc/hosts
-echo ::1		localhost >> /etc/hosts
-echo 127.0.1.1	myhostname.localdomain	myhostname /etc/hosts
-
-pacman -S grub efibootmgr 
-mkdir /mnt/boot/EFI
-mount /dev/sda1 /mnt/boot/EFI
-grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-
-pacman -S dhcpcd --noconfirm
-systemctl start dhcpcd
-systemctl enable dhcpcd
+arch-chroot /mnt ./installChroot.sh
