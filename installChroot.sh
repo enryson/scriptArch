75bd916daf41b7192c4efd8441f18cbb0a2ad115
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-12 23:40:30 -0300

    update

--- installChroot.sh
+++ installChroot.sh
@@ -1,19 +1,3 @@
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.fat -F32 /dev/sda1
-
-mount /dev/sda3 /mnt
-
-pacman -Syy
-pacman -S reflector --noconfirm
-reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syy
-
-pacstrap /mnt base linux linux-firmware base-devel
-genfstab -U /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt
 pacman -Syy
 pacman -S reflector --noconfirm
 reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
