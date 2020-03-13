ace084f782c1f8cc4f50d17e5d8c7be15e3310e4
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-12 23:01:53 -0300

    Fix

--- install.sh
+++ install.sh
@@ -35,5 +35,6 @@ mount /dev/sda1 /mnt/boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo systemctl start dhcpcd
-sudo systemctl enable dhcpcd
+pacman -S dhcpcd
+systemctl start dhcpcd
+systemctl enable dhcpcd
