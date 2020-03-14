4f8bacaa3bf44d97469b11a3d73610a852d6b8bf
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-13 22:51:36 -0300

    f

--- install.sh
+++ install.sh
@@ -5,23 +5,28 @@ mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
 mkfs.fat -F32 /dev/sda1
+clear
 
+echo "Montando Sistema"
 mkdir /mnt/boot/EFI
 mount /dev/sda1 /mnt/boot/EFI
-
 mount /dev/sda3 /mnt
+clear
 
 echo "Baixando Reflector"
 pacman -Syy
 pacman -S reflector git --noconfirm
 clear
+
 echo "Atualizando mirrors...."
 reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
 
-clear
+echo "Instalando o Sistema BASE"
 pacstrap /mnt base linux linux-firmware base-devel nano git
 genfstab -U /mnt >> /mnt/etc/fstab
+clear
+
 echo "Copiando Mirrors para Sistema"
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 clear
--- installChroot.sh
+++ installChroot.sh
@@ -2,10 +2,10 @@ echo "Script numero 2"
 delay 200
 clear
 
+echo "Copiando Arquivos de configuração"
 echo [multilib] >>  /etc/pacman.conf 
 echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf
 
-pacman -Syy
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 
@@ -20,22 +20,34 @@ echo ArchLinux >> /etc/hostname
 echo 127.0.0.1	localhost >> /etc/hosts
 echo ::1		localhost >> /etc/hosts
 echo 127.0.1.1	myhostname.localdomain	myhostname /etc/hosts
+clear
 
-pacman -S grub efibootmgr --noconfirm
+echo "Atualizando Repositórios"
+pacman -Syy
+clear
+
+echo "Instalando Grub EFI e dhcpcd"
+pacman -S grub efibootmgr dhcpcd --noconfirm
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+clear
 
-pacman -S dhcpcd --noconfirm
+echo "habilitando o dhcpcd"
 systemctl start dhcpcd
 systemctl enable dhcpcd
+clear
 
 echo "download script git"
 pacman -S git --noconfirm
 git clone https://github.com/enryson/scriptArch.git
 tar xvf scriptArch.*
 
+echo "Copiando Script de boot"
 cp /scriptArch/files/postInstall.sh /usr/bin/postInstall.sh
 sudo chmod 755 /usr/bin/postInstall.sh
-
 cp /scriptArch/files/afterInstall.service /etc/systemd/system/afterInstall.service
 sudo systemctl enable myscript.service
+echo "Reiniciando"
+umount /dev/sda1
+umount /dev/sda3
+reboot
