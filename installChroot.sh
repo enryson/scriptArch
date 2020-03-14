40462ca63f094931430feb9f7d5a29add3023e23
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-13 22:26:11 -0300

    fix locale.gen

--- installChroot.sh
+++ installChroot.sh
@@ -13,7 +13,7 @@ ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-echo en_US ISO-8859-1 >> //etc/locale.gen
+echo en_US ISO-8859-1 >> /etc/locale.gen
 
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 export LANG=en_US.UTF-8
