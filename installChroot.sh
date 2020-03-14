86fe2ecd3c3a834fc2dbce82e5177d9adb68d733
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-13 22:41:59 -0300

    c

--- installChroot.sh
+++ installChroot.sh
@@ -2,28 +2,28 @@ echo "Script numero 2"
 delay 200
 clear
 
-echo [multilib] >>  /etc/pacman.conf 
-echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf
+# echo [multilib] >>  /etc/pacman.conf 
+# echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf
 
-pacman -Syy
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
+# pacman -Syy
+# ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+# hwclock --systohc
 
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-echo en_US ISO-8859-1 >> /etc/locale.gen
+# echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+# echo en_US ISO-8859-1 >> /etc/locale.gen
 
-echo LANG=en_US.UTF-8 >> /etc/locale.conf
-export LANG=en_US.UTF-8
-locale-gen
+# echo LANG=en_US.UTF-8 >> /etc/locale.conf
+# export LANG=en_US.UTF-8
+# locale-gen
 
-echo ArchLinux >> /etc/hostname
-echo 127.0.0.1	localhost >> /etc/hosts
-echo ::1		localhost >> /etc/hosts
-echo 127.0.1.1	myhostname.localdomain	myhostname /etc/hosts
+# echo ArchLinux >> /etc/hostname
+# echo 127.0.0.1	localhost >> /etc/hosts
+# echo ::1		localhost >> /etc/hosts
+# echo 127.0.1.1	myhostname.localdomain	myhostname /etc/hosts
 
-pacman -S grub efibootmgr --noconfirm
-grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+# pacman -S grub efibootmgr --noconfirm
+# grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+# grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S dhcpcd --noconfirm
 systemctl start dhcpcd
