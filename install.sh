1e2d5c3003b5f9babaf9134f9ac783e1b50dc049
Enryson Oliveira Ferraz <enryson@gmail.com>
2020-03-13 23:24:49 -0300

    fix

--- install.sh
+++ install.sh
@@ -5,30 +5,37 @@ mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
 mkfs.fat -F32 /dev/sda1
+delay 4000
 clear
 
 echo "Montando Sistema"
 mkdir /mnt/boot/EFI
 mount /dev/sda1 /mnt/boot/EFI
 mount /dev/sda3 /mnt
+delay 4000
 clear
 
 echo "Baixando Reflector"
 pacman -Syy
 pacman -S reflector git --noconfirm
+delay 4000
 clear
 
 echo "Atualizando mirrors...."
 reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
+delay 4000
+clear
 
 echo "Instalando o Sistema BASE"
 pacstrap /mnt base linux linux-firmware base-devel nano git
 genfstab -U /mnt >> /mnt/etc/fstab
+delay 4000
 clear
 
 echo "Copiando Mirrors para Sistema"
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+delay 4000
 clear
 
 echo "Baixando novo Script"
@@ -36,3 +43,7 @@ wget https://raw.githubusercontent.com/enryson/scriptArch/master/installChroot.s
 cp installChroot.sh /mnt
 chmod +x /mnt/installChroot.sh
 arch-chroot /mnt ./installChroot.sh
+
+echo "Reiniciando....."
+delay 14000
+reboot
